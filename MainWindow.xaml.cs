59936935b2e1e8ac35a4592de1b5e9de62a1a93b
qaz wsx <cgeocodgod@gmail.com>
2020-04-24 20:24:04 +0300

    + Power actions menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Forms;
 using MahApps.Metro.Controls;
 using gamepad_mouse_controller.Model;
+using System.Diagnostics;
 
 namespace gamepad_mouse_controller
 {
@@ -46,9 +47,22 @@ namespace gamepad_mouse_controller
             
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ShutdownButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            MessageBoxResult result = System.Windows.MessageBox.Show("Are you sure you want to turn off the system?", "Turn off the system", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if(result == MessageBoxResult.Yes)
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = System.Windows.MessageBox.Show("Are you sure you want to restart the system?", "Restart the system", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                Process.Start("shutdown", "/r /t 0");
+            }
         }
     }
 }
